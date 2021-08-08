48506a62d6d58d15eacba8941de1ee394f132d1c
JohnYang524 <john.yangzhe@gmail.com>
2021-08-07 23:29:11 -0700

    Added Contacts as app dependency.

--- twilio-sdk-coding-challenge-android-0.2.0/twilio-sdk-coding-challenge-android-0.2.0/app/src/main/java/com/contacts/app/ContactsActivity.java
+++ twilio-sdk-coding-challenge-android-0.2.0/twilio-sdk-coding-challenge-android-0.2.0/app/src/main/java/com/contacts/app/ContactsActivity.java
@@ -3,6 +3,7 @@ package com.contacts.app;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +11,7 @@ import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.contacts.Contacts;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
@@ -52,6 +54,7 @@ public class ContactsActivity extends AppCompatActivity {
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), Contacts.getVersion(), Toast.LENGTH_SHORT).show();
                 createNewContact();
             }
         });
