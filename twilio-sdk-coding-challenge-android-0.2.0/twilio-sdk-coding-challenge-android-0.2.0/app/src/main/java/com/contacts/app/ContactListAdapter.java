beb80c827a9f526a8b1485456449e483507c40b3
JohnYang524 <john.yangzhe@gmail.com>
2021-08-08 17:29:45 -0700

    Adapter code cleaned up.

--- twilio-sdk-coding-challenge-android-0.2.0/twilio-sdk-coding-challenge-android-0.2.0/app/src/main/java/com/contacts/app/ContactListAdapter.java
+++ twilio-sdk-coding-challenge-android-0.2.0/twilio-sdk-coding-challenge-android-0.2.0/app/src/main/java/com/contacts/app/ContactListAdapter.java
@@ -30,10 +30,8 @@ public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.
     public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
 
-        // Inflate the custom layout
         View contactView = inflater.inflate(R.layout.row_item_contact, parent, false);
 
-        // Return a new holder instance
         ViewHolder viewHolder = new ViewHolder(contactView);
         return viewHolder;
     }
@@ -43,7 +41,6 @@ public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.
         // Get the data model based on position
         Contact contact = mContacts.get(position);
 
-        // Set item views based on your views and data model
         TextView nameTextView = holder.nameTextView;
         TextView phoneTextView = holder.phoneTextView;
         nameTextView.setText(contact.getName());
@@ -58,16 +55,10 @@ public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.
     // Provide a direct reference to each of the views within a data item
     // Used to cache the views within the item layout for fast access
     public class ViewHolder extends RecyclerView.ViewHolder {
-        // Your holder should contain a member variable
-        // for any view that will be set as you render a row
         public TextView nameTextView;
         public TextView phoneTextView;
 
-        // We also create a constructor that accepts the entire item row
-        // and does the view lookups to find each subview
         public ViewHolder(View itemView) {
-            // Stores the itemView in a public final member variable that can be used
-            // to access the context from any ViewHolder instance.
             super(itemView);
 
             nameTextView = (TextView) itemView.findViewById(R.id.tv_contact_name);
