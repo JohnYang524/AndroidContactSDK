6ab228e9681a53ae7b809b1e7863ecec5d596d84
JohnYang524 <john.yangzhe@gmail.com>
2021-08-08 20:07:37 -0700

    Cleaned up Contacts code

--- twilio-sdk-coding-challenge-android-0.2.0/twilio-sdk-coding-challenge-android-0.2.0/contacts/src/main/java/com/contacts/Contacts.java
+++ twilio-sdk-coding-challenge-android-0.2.0/twilio-sdk-coding-challenge-android-0.2.0/contacts/src/main/java/com/contacts/Contacts.java
@@ -14,8 +14,7 @@ import java.util.List;
  * 2. Add a new contact.
  * 3. Notify user of an updated contact.
  *
- * The class includes a simple [.getVersion] method to demonstrate how to call from a
- * Java class to C++ code.
+ *  Singleton class for making Contacts API calls.
  */
 public class Contacts {
     private static final boolean mIsDebuggable = true; // TODO: use value in config file
@@ -71,104 +70,4 @@ public class Contacts {
     public void syncContactData(Context context) {
         manager.syncContactList(context);
     }
-
-//     /**
-//      * Update local contact list data.
-//      * TODO: use a better logic to update local list. E.g. update list after server call before updating DB
-//      * For now, update local list with Room DB data
-//     */
-//    public void updateLocalContactList(Context context) {
-//
-//        ContactDatabase appDb = ContactDatabase.getInstance(context);
-//        List<Contact> dbList = appDb.contactDao().getAll();
-//        mContactList.clear();
-//        mContactList.addAll(dbList);
-//
-//        /*if (list.isEmpty()) {
-//            mContactList.addAll(list);
-//        }
-//        else { // Make changes to the newly updated contact record
-//            for (Contact updatedContact : list) {
-//                if (mContactList.contains(updatedContact)) { // Update existing record
-//                    mContactList.remove(updatedContact);
-//                }
-//                mContactList.add(updatedContact);
-//            }
-//        }*/
-//        if (mIsDebuggable)
-//            Log.v(TAG, "Updated local contact list. Current length: " + mContactList.size());
-//    }
-
-    // Check server update time before retrieving contact list and sync DB
-   /* public void syncContactList(Context context) {
-        Executor executor = Executors.newSingleThreadExecutor();
-        Handler handler = new Handler(Looper.getMainLooper());
-        executor.execute(new Runnable() {
-            @Override
-            public void run() {
-                syncContactListInBackground(context);
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        // do UI changes after background work
-                        if (mEventListener != null) {
-                            mEventListener.onContactListLoaded();
-                        }
-                    }
-                });
-            }
-        });
-    }
-
-    // Step 1: Retrieve last update time in server
-    // Step 2: Based on last sync timestamp, the list is updated either from Room DB,
-    // or from Web service call responses from C++ SDK
-    // Step 3: If list is updated with server data, update local DB
-    public void syncContactListInBackground(Context context) {
-        String lastUpdateTimeInServer = getLastUpdateTimeFromServer();
-        if (Util.localDBUpToDate(context, lastUpdateTimeInServer)) {// DB up-to-date, get contact list from Room DB
-            if (mIsDebuggable)
-                Log.v(TAG, "Local DB up to date, getting data from local DB");
-            updateLocalContactList(context);
-        } else { // DB not up-to-date, sync DB with server data
-            if (mIsDebuggable)
-                Log.v(TAG, "Local DB NOT up to date, getting data from server");
-            syncContactDataFromServer(context);
-        }
-    }
-
-    // TODO: update UI before syncing DB to improve performance
-    private void syncContactDataFromServer(Context context) {
-        // Get timestamp for last Room DB update
-        String lastDBUpdateTimestamp = Util.getLastSyncTime(context);
-        // Get list of NEW/Updated Contacts by making call to C++ SDK
-        String jsonResponse = getNewlyUpdatedContactListFromServer(lastDBUpdateTimestamp);
-        // Parse JSON string to a list of Contacts, and update local list and DB
-        List<Contact> updatedContactList = ContactListResponse.parseJSON(jsonResponse).contactList;
-//        updateLocalContactList(updatedContactList);
-        // Update Room DB to match server data
-        syncLocalDB(context, updatedContactList);
-        // Update DB update timestamp
-        // TODO: change "0" to be current timestamp, hardcoding the value here for testing only
-        Util.saveLastSyncTime(context, "0");
-        // Update local list
-        updateLocalContactList(context);
-    }
-
-    // Update Room DB to match server data
-    // TODO: use sync adapters to improve db data modification performance
-    private void syncLocalDB(Context context, List<Contact> contactList) {
-        ContactDatabase appDb = ContactDatabase.getInstance(context);
-//        Contact[] contactsArray = new Contact[mContactList.size()];
-//        appDb.contactDao().insertAll(contactsArray);
-        for (Contact contact : contactList) {
-            Contact dbRecord = appDb.contactDao().findById(contact.id);
-            if (dbRecord != null) {
-                // Delete existing record
-                appDb.contactDao().delete(dbRecord);
-            }
-            // insert new record
-            appDb.contactDao().insertAll(new Contact[]{contact});
-        }
-    }*/
 }
