fbcb41a47ba7b8fd7c810d877a014e1ab6ba93b1
BPI\jyang <john.yang@zovio.com>
2021-08-11 14:48:30 -0700

    Updated logging

--- twilio-sdk-coding-challenge-android-0.2.0/twilio-sdk-coding-challenge-android-0.2.0/app/src/main/java/com/contacts/app/helpers/ContactActivityObserver.java
+++ twilio-sdk-coding-challenge-android-0.2.0/twilio-sdk-coding-challenge-android-0.2.0/app/src/main/java/com/contacts/app/helpers/ContactActivityObserver.java
@@ -7,15 +7,16 @@ import androidx.lifecycle.LifecycleObserver;
 import androidx.lifecycle.OnLifecycleEvent;
 
 public class ContactActivityObserver implements LifecycleObserver {
+    private static final boolean mIsDebuggable = true; // TODO: use value in config file
     private String TAG = this.getClass().getSimpleName();
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     public void onCreateEvent() {
-        Log.v(TAG, "onCreateEvent");
+        if (mIsDebuggable) Log.v(TAG, "onCreate Event");
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     public void onResumeEvent() {
-        Log.v(TAG, "OnResume Event");
+        if (mIsDebuggable) Log.v(TAG, "OnResume Event");
     }
 }
